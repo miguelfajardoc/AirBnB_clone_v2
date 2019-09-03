5632fc45ad08d9d39043ad9ce9b32665fd4ab52a
miguelfajardoc <775@holbertonschool.com>
2019-09-03 03:26:54 +0000

    flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module hello flask"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_HBNB():
+    return "Hello HBNB!"
+
+app.debug = True
+app.run(host='0.0.0.0', port=5000)
