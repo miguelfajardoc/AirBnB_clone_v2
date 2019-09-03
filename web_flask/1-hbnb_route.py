bfb3f6179a09e392779262080b146a87d367a61c
miguelfajardoc <775@holbertonschool.com>
2019-09-03 17:37:51 +0000

    route to C

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def hello_HBNB():
     return "Hello HBNB!"
 
+@app.route('/hbnb')
+def HBNB():
+    return "HBNB"
+
 app.debug = True
 app.run(host='0.0.0.0', port=5000)
 app.url_map.strict_slashes = False
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,6 +10,15 @@ app = Flask(__name__)
 def hello_HBNB():
     return "Hello HBNB!"
 
+@app.route('/hbnb')
+def HBNB():
+    return "HBNB"
+
+@app.route('/c/<text>')
+def C_is_fun(text):
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
 app.debug = True
 app.run(host='0.0.0.0', port=5000)
 app.url_map.strict_slashes = False
