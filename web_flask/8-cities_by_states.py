644cc389290744fa842cf38f790efc5d862073be
miguelfajardoc <775@holbertonschool.com>
2019-09-05 02:50:49 +0000

    ???

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,17 +13,16 @@ app.url_map.strict_slashes = False
 def states():
     states = storage.all(State)
     cities_by_states = {}
-    iter = 0
+    states_l = {}
+
     for state in states:
         cities_by_states[states[state].id] = states[state].cities
 
-    states_l = []
-    for key, values in states.items():
-        tup = (values.id, values.name)
-        states_l.append(tup)
-        states_l.sort(key=operator.itemgetter(1))
-    return render_template('8-cities_by_states.html', states=states_l,
-                           cities=cities_by_states)
+
+    for key in states:
+        states_l[states[key].id] = states[key].name
+
+    return render_template('8-cities_by_states.html', states=states_l, cities=cities_by_states)
 
 
 @app.teardown_appcontext
