e4a9ad38be5a38d9b4726de79d5abeed01e4b769
miguelfajardoc <775@holbertonschool.com>
2019-09-06 21:37:09 +0000

    states by cities all in jinja

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,17 +12,8 @@ app.url_map.strict_slashes = False
 @app.route('/cities_by_states')
 def states():
     states = storage.all(State)
-    cities_by_states = {}
-    states_l = {}
 
-    for state in states:
-        cities_by_states[states[state].id] = states[state].cities
-
-    for key in states:
-        states_l[states[key].id] = states[key].name
-
-    return render_template('8-cities_by_states.html', state=states_l,
-                           city=cities_by_states)
+    return render_template('8-cities_by_states.html', states=states )
 
 
 @app.teardown_appcontext
